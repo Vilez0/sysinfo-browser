7c16c255bc8136eb3405a2de4c1a0da578fd067b
Edip1 <medip642@gmail.com>
2023-08-14 13:02:36 +0300

    Add a function for get the cpu usage in last x seconds from database

--- src/main.go
+++ src/main.go
@@ -4,17 +4,23 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/shirou/gopsutil/cpu"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+
+	"strconv"
+	"strings"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/shirou/gopsutil/cpu"
 )
 
 type httpHandlerFunc func(http.ResponseWriter, *http.Request)
+
+const database string = "../db/usage.db"
 
 var (
 	InfoLogger  *log.Logger
@@ -31,11 +37,11 @@ func init() {
 	ErrorLogger = log.New(mw, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 func main() {
-	http.Handle("/", withExecTime(getIndex))
-	http.Handle("/index.mjs", withExecTime(getIndexjs))
-	http.Handle("/index.css", withExecTime(getIndexCss))
-	http.Handle("/realtime/cpus", withExecTime(cpusGet))
-	storeCpuUsageEverySecond()
+	go storeCpuUsageEverySecond()
+	http.Handle("/", withExecTime(serveIndex))
+	http.Handle("/index.mjs", withExecTime(serveIndexjs))
+	http.Handle("/index.css", withExecTime(serveIndexCss))
+	http.Handle("/realtime/cpus/", withExecTime(serveCpuUsage))
 	InfoLogger.Println("starting server")
 	err := http.ListenAndServe(":7052", nil)
 	if err != nil {
@@ -44,16 +50,17 @@ func main() {
 
 }
 
-func getIndex(w http.ResponseWriter, r *http.Request) {
+func serveIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-func getIndexCss(w http.ResponseWriter, _ *http.Request) {
+func serveIndexCss(w http.ResponseWriter, _ *http.Request) {
 	file, err := os.ReadFile("index.css")
 	if err != nil {
 		ErrorLogger.Printf("error reading index.css file: %v", err)
 		return
 	}
+
 	w.Header().Set("Content-Type", "text/css;charset=utf-8")
 	_, err = w.Write(file)
 	if err != nil {
@@ -61,18 +68,35 @@ func getIndexCss(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func getIndexjs(w http.ResponseWriter, _ *http.Request) {
+func serveIndexjs(w http.ResponseWriter, _ *http.Request) {
 	file, err := os.ReadFile("./index.mjs")
 	if err != nil {
 		ErrorLogger.Printf("error reading index.mjs file: %v", err)
 		return
 	}
+
 	w.Header().Set("content-type", "application/javascript;charset=utf-8")
 	_, err = w.Write(file)
 	if err != nil {
 		return
 	}
 }
+
+func serveCpuUsage(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.RequestURI()
+
+	if url == "/realtime/cpus/" || url == "/realtime/cpus" {
+		fmt.Fprint(w, string(getCpuUsage()))
+	} else {
+		seconds, err := strconv.Atoi(strings.Split(url, "/realtime/cpus/")[1])
+		if err != nil {
+			ErrorLogger.Printf("error when converting string to integar: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "%v\n", getCpuUsageLastSeconds(seconds))
+	}
+}
+
 func getCpuUsage() []byte {
 	percent, err := cpu.Percent(0, true)
 	if err != nil {
@@ -85,9 +109,6 @@ func getCpuUsage() []byte {
 	}
 	return e
 }
-func cpusGet(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "%s\n", getCpuUsage())
-}
 
 func storeCpuUsageEverySecond() {
 	for range time.Tick(time.Second * 1) {
@@ -96,8 +117,8 @@ func storeCpuUsageEverySecond() {
   time INTEGER NOT NULL PRIMARY KEY,
   usage BLOB NOT NULL
   );`
-		cpuUsage := fmt.Sprintf("%s", getCpuUsage())
-		db, err := sql.Open("sqlite3", "../db/usage.db")
+		cpuUsage := string(getCpuUsage())
+		db, err := sql.Open("sqlite3", database)
 		if err != nil {
 			ErrorLogger.Printf("database error %v", err)
 			return
@@ -113,7 +134,41 @@ func storeCpuUsageEverySecond() {
 			return
 		}
 	}
+}
 
+func getCpuUsageLastSeconds(seconds int) []float64 {
+	db, err := sql.Open("sqlite3", database)
+	if err != nil {
+		ErrorLogger.Printf("Cannot open database: %v", err)
+	}
+
+	data, err := db.Query("SELECT usage FROM CPU_USAGE WHERE time >= UNIXEPOCH('now','-" + strconv.Itoa(seconds) + " seconds');")
+	if err != nil {
+		ErrorLogger.Printf("Cannot extract usage data from database: %v", err)
+	}
+
+	defer data.Close()
+	var cpuUsages []float64
+	for data.Next() {
+		var usage string
+		err = data.Scan(&usage)
+		if err != nil {
+			ErrorLogger.Printf("Cannot scan database: %v\n", err)
+		}
+
+		usage = strings.Replace(usage, "[", "", -1)
+		usage = strings.Replace(usage, "]", "", -1)
+		usages := strings.Split(usage, ",")
+
+		for element := range usages {
+			toFloat64, err := strconv.ParseFloat(usages[element], 32)
+			if err != nil {
+				ErrorLogger.Printf("Cannot convert string to float64: %v\n", err)
+			}
+			cpuUsages = append(cpuUsages, toFloat64)
+		}
+	}
+	return cpuUsages
 }
 
 func withExecTime(hf httpHandlerFunc) http.Handler {
